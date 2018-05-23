408e47e7d7a2ef53296f1b7ae3bbe10c0fd7c9da
Francois Stephany <francois@tamere.eu>
2018-05-22 23:00:46 -0400

    Add Timber as external lib to the android project

--- App-A-Android/src/main/java/be/tulipemoutarde/appa/MainActivity.java
+++ App-A-Android/src/main/java/be/tulipemoutarde/appa/MainActivity.java
@@ -2,11 +2,14 @@ package be.tulipemoutarde.appa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import timber.log.Timber;
+import static timber.log.Timber.DebugTree;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Timber.plant(new DebugTree());
         setContentView(R.layout.activity_main);
     }
 }
--- WORKSPACE
+++ WORKSPACE
@@ -1,7 +1,14 @@
 # Android
-
 android_sdk_repository(
     name = "androidsdk",
     api_level = 27,
     build_tools_version = "27.0.3",
+)
+
+# External deps for android
+load("@bazel_tools//tools/build_defs/repo:maven_rules.bzl", "maven_aar")
+
+maven_aar(
+    name = "timber",
+    artifact= "com.jakewharton.timber:timber:4.7.0"
 )
